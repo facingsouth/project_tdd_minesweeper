1e11dea0a75437954708f9e828b28d48d30ecfd8
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-03 15:01:22 -0700

    init commit

---
+++ lib/board.rb
@@ -0,0 +1,29 @@
+class Board
+
+
+  def initialize
+
+    @gamestate = Array.new(10) {Array.new(10) {[0,'']}}
+
+
+  end
+
+
+  def render
+
+    @gamestate.each {|i| p i}
+
+  end
+                
+
+
+
+
+
+end
+
+
+
+ms = Board.new
+
+ms.render
